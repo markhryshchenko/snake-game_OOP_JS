f775e11d0c6e11559520ea18eb67e107a4939237
markhryshchenko <grishenko.ua@gmail.com>
2022-11-15 14:28:55 +0200

    Updates

---
+++ scripts/score.js
@@ -0,0 +1,19 @@
+export default class Score {
+    constructor(scoreBlock, score = 0 ){
+        this.scoreBlock = document.querySelector(scoreBlock);
+        this.score = score;
+        this.draw();
+    }
+    incScore(){
+        this.score++;
+        this.draw();
+
+    }
+    setToZero(){
+        this.score = 0;
+        this.draw();
+    }
+    draw(){
+        this.scoreBlock.innerHtml = this.score;
+    }
+}
